872336fc8bda9ec7f23ae307cb05e6e84f8765a4
705H-S <93358442+705h-S@users.noreply.github.com>
2022-03-01 20:47:24 -0800

    finished User Schema

--- models/User.js
+++ models/User.js
@@ -1,32 +1,58 @@
 // https://mongoosejs.com/docs/guide.html
 import mongoose from "mongoose";
 
-const {Schema, model} = mongoose;
+const { Schema, model } = mongoose;
 
 // using regex to validate email
-var validateEmail = function(email) {
-    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    return re.test(email)
+var validateEmail = function (email) {
+  var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email);
 };
 
-// defining schema 
+// defining schema
 const userSchema = new Schema(
-    // properties
-    {
-        userName: {
-            type: String,
-            unique: true,
-            required: true,
-            trim: true
-        },
-        email: {
-            type: String, 
-            unique: true,
-            required: true,
-            validate: [ validateEmail, 'Please fill a valid email address']
-        },
-    }
+  // properties
+  {
+    userName: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      validate: [validateEmail, "Please fill a valid email address"],
+    },
+    // --- Subdocument---
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    // --- end ---
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
 
+// virtual;  this won't save into model or db but it will load onto the html
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
 
-
-)
+// telling other files to look into this file for schema
+const User = model("User", userSchema);
+module.exports = User;
