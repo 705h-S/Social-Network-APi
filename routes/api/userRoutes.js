7746dfaa296c6156fe77b8ccabfb5295266f6b86
705H-S <93358442+705h-S@users.noreply.github.com>
2022-03-04 17:58:18 -0800

    added user routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -10,5 +10,24 @@ const {
     removeAmigo,
 } = require('../../controllers/userController');
 
+router.route('/')
+  .get(allUsers)
+  .post(createUser);
 
+// /api/users/:userId 
+router.route('/:userId')
+  .get(getAUser)
+  .put(updateUser)
+  .delete(deleteUser);
+
+// /api/users/:userId /friends 
+router.route('/:userId/friends')
+  .post(addAmigo);
+
+// /api/users/:userId /friends/:friendId 
+router.route('/:userId/friends/:friendId')
+  .delete(removeAmigo);
+
+// Export module router
+module.exports = router;
 
