ac93a5217df8c2e8ad6dafde372681e0b92a6f0c
705H-S <93358442+705h-S@users.noreply.github.com>
2022-03-04 18:03:04 -0800

    added thought routes

--- routes/api/thoughtRouter.js
+++ routes/api/thoughtRouter.js
@@ -1,12 +1,24 @@
-const router = require('express').Router();
-// getting controllers 
+const router = require("express").Router();
+// getting controllers
 const {
-    getThoughts,
-    getSingleThought,
-    createThought,
-    updateThought,
-    deleteThought,
-    createReaction,
-    deleteReaction,
-  } = require('../../controllers/thoughtsController');
-  
+  getThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  createReaction,
+  deleteReaction,
+} = require("../../controllers/thoughtsController");
+
+router.route("/").get(getAllThoughts).post(createThought);
+
+router
+  .route("/:thoughtId")
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+router.route("/:thoughtId/reactions").post(createReaction);
+router.route("/:thoughtId/reactions/:reactionId").delete(deleteReaction);
+
+module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,33 +1,25 @@
-const router = require('express').Router();
+const router = require("express").Router();
 // getting all controllers
 const {
-    allUsers,
-    getAUser,
-    createUser,
-    updateUser,
-    deleteUser,
-    addAmigo,
-    removeAmigo,
-} = require('../../controllers/userController');
+  allUsers,
+  getAUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addAmigo,
+  removeAmigo,
+} = require("../../controllers/userController");
 
-router.route('/')
-  .get(allUsers)
-  .post(createUser);
+router.route("/").get(allUsers).post(createUser);
 
-// /api/users/:userId 
-router.route('/:userId')
-  .get(getAUser)
-  .put(updateUser)
-  .delete(deleteUser);
+// /api/users/:userId
+router.route("/:userId").get(getAUser).put(updateUser).delete(deleteUser);
 
-// /api/users/:userId /friends 
-router.route('/:userId/friends')
-  .post(addAmigo);
+// /api/users/:userId /friends
+router.route("/:userId/friends").post(addAmigo);
 
-// /api/users/:userId /friends/:friendId 
-router.route('/:userId/friends/:friendId')
-  .delete(removeAmigo);
+// /api/users/:userId /friends/:friendId
+router.route("/:userId/friends/:friendId").delete(removeAmigo);
 
 // Export module router
 module.exports = router;
-
