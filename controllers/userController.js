fc7f40a96fc3b1788778b8e34a5fc60e487e75dd
705H-S <93358442+705h-S@users.noreply.github.com>
2022-03-04 17:15:01 -0800

    create get all users and single user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,2 +1,29 @@
 // telling to look in models
 const { User, Thought } = require('../models');
+
+module.exports = {
+    // retreave Users
+    allUsers(req, res) {
+        User.find()
+          
+          .populate({path: 'thoughts', select: '-__v'})
+          .populate({path: 'friends', select: '-__v'})
+          .select('-__v')
+          .then((userData) => res.json(userData))
+          .catch((err) => res.status(500).json(err));
+      },
+      // single user
+      getAUser(req, res) {
+        User.findOne({ _id: req.params.userId })
+          .populate({path: 'thoughts', select: '-__v'})
+          .populate({path: 'friends', select: '-__v'})
+          .select('-__v')
+          .then((userData) =>
+            !userData
+              ? res.status(404).json({ message: 'No user with that ID' })
+              : res.json(userData)
+          )
+          .catch((err) => res.status(500).json(err));
+      },
+
+}
