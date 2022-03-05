e5f08de40ff15191072ba075e7844d8ff7e003d8
705H-S <93358442+705h-S@users.noreply.github.com>
2022-03-04 18:06:55 -0800

    added index for api

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,1 +1,12 @@
 const router = require('express').Router();
+
+const userRoutes = require('./userRoutes');
+const thoughtRoutes = require('./thoughtRouter');
+
+router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
+
+module.exports = router;
+
+const router = require('express').Router();
+
